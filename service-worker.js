a942d1ee78c584bdfbe554741463da4e35f0f098
Stefan Sundin <git@stefansundin.com>
2022-03-06 14:34:10 -0800

    Add service worker.

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// Increment this number to trigger offline clients to update their caches:
+// v1
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches
+      .open('maxmind-db.js')
+      .then(c =>
+        c.addAll([
+          'https://cdn.jsdelivr.net/gh/stefansundin/maxmind-db.js@v0.0.2/dist/MaxMindDB.min.js',
+          'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css',
+          'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js',
+          'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js',
+          'https://cdn.jsdelivr.net/gh/nodeca/pako@v2.0.4/dist/pako.min.js',
+          'https://cdn.jsdelivr.net/gh/InvokIT/js-untar@v2.0.0/build/dist/untar.js',
+          'https://cdn.jsdelivr.net/gh/Stuk/jszip@v3.7.1/dist/jszip.min.js',
+          'demo.css',
+          'demo.js',
+          'icon.png',
+        ]),
+      ),
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // console.log(e.request.url);
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# To test the PWA app you have to serve the page from https://.
+# Use nginx.sh to do that instead of this.
+
 echo "http://localhost:8000/"
 set -x
 python3 -m http.server
